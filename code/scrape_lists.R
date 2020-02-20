c8fb5085a43c4e93fac513755a82ab99bd3e144e
jasonmhoule <jasonmhoule@gmail.com>
2020-02-20 22:43:15 +0000

    Scraping - handle <br>s and cleanup excessive line breaks

--- code/scrape_lists.R
+++ code/scrape_lists.R
@@ -1,5 +1,6 @@
 library(RSelenium)
 library(rvest)
+library(tidyverse)
 remDr <- remoteDriver(remoteServerAddr = "selenium", port = 4444L, browserName = "chrome")
 remDr$open()
 
@@ -19,26 +20,18 @@ lists_raw <- page %>%
   html_nodes(".list") %>% 
   html_text()
 
-lists <- lists_raw[c(1,3:8)]  
-  
-str_split(lists[[1]],"\n")
-str_replace_all(lists, "\n\n", "\n") %>% 
+lists_raw <- page %>% 
+  html_nodes(".list") %>% 
+  as.character() %>% 
+  str_replace_all("<br>","\n") %>% 
+  str_replace_all("<.*?>","") %>% 
+  str_replace_all("[\n]{1,}","\n") %>% 
+  str_replace_all("^\n","") %>% 
   str_split("\n")
 
-lists_raw2 <- page %>% 
-  html_nodes(".list")
+lists <- lists_raw[c(1,3:8)]  
 
-lists_raw2[[1]] %>%
-  html_name()
+lists[[1]]
 
-str_replace_all(lists_raw2[[1]],"<.*?>")
 
-## ??? ###
-x <- read_xml("<root>
-  <a><b>1</b> <b>2</b></a>
-  <a><b>3</b></a>
-</root>")
 
-x %>% 
-  xml_find_all("a") %>% 
-  map_chr(. %>% xml_find_all("b") %>% xml_text() %>% paste(collapse = ", "))
